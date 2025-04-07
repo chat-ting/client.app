b10b2cb2a7a4787bd22fcabf740511b4823f3f30
vvyre <seung.yoon.yu@gmail.com>
2025-04-07 17:57:47 +0900

    feat: 테마 컬러 추가

---
+++ _lib/colors.ts
@@ -0,0 +1,8 @@
+const THEME = 'rgba(97, 211, 112,1)';
+export const COLORS = {
+  THEME,
+  WHITE: 'rgba(255,255,255,1)',
+  BLACK: 'rgba(0,0,0,1)',
+  TEXT: 'rgba(33,33,33,1)',
+  CAPTION: 'rgba(155,155,155,1)',
+};
